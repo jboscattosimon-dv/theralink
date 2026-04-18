6fb4e190f0c191fb10aa2823c67e92c3bceb7dfb
laauradanieli-lgtm <laaura.danieli@gmail.com>
2026-04-18 01:06:40 -0300

    Redireciona raiz para index.html

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const io = new Server(server, { cors: { origin: '*' } });
 app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
+app.get('/', (req, res) => res.redirect('/index.html'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/pacientes', pacientesRoutes);
